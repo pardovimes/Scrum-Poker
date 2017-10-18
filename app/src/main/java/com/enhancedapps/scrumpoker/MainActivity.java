0ebafece11879aab92d34546442dc06ddee1b84d
pardtheman@gmail.com <pardtheman@gmail.com>
2017-10-18 23:21:38 +0200

    Layout test

--- app/src/main/java/com/enhancedapps/scrumpoker/MainActivity.java
+++ app/src/main/java/com/enhancedapps/scrumpoker/MainActivity.java
@@ -2,6 +2,8 @@ package com.enhancedapps.scrumpoker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i = 1; i<=5; i++){
+
+        }
     }
 }
