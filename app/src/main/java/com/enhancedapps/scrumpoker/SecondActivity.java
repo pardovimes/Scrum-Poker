d9e5802551e1d3ef337af1aeb48dd3b32f06bc28
pardtheman@gmail.com <pardtheman@gmail.com>
2017-10-19 23:17:58 +0200

    Rotate3dAnimation implement between intents

---
+++ app/src/main/java/com/enhancedapps/scrumpoker/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.enhancedapps.scrumpoker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    protected void onResume() {
+        // animateIn this activity
+        ActivitySwitcher.animationIn(findViewById(R.id.container),
+                getWindowManager());
+        super.onResume();
+    }
+}
