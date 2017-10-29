6a900aefe8a995a6b8d6e3497147d29a1fa8ad0d
pardtheman@gmail.com <pardtheman@gmail.com>
2017-10-29 22:04:55 +0100

    Fragments creation

---
+++ app/src/main/java/com/enhancedapps/scrumpoker/Fragments/CardFragment.java
@@ -0,0 +1,27 @@
+package com.enhancedapps.scrumpoker.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.enhancedapps.scrumpoker.R;
+
+public class CardFragment extends Fragment {
+
+
+    public CardFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_card, container, false);
+
+        return view;
+    }
+
+}
